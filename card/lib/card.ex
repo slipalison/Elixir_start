3e2249311b12ce09f3ad3734e7ca2e3537e2cbb8
slipalison <alisonmsn@hotmail.com>
2017-12-05 18:39:18 -0200

    aula 15

--- card/lib/card.ex
+++ card/lib/card.ex
@@ -15,4 +15,35 @@ defmodule Card do
   def hello do
     :world
   end
+
+  def create_deck do
+    values = ["Ace", "Two", "Tree", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonts"]
+
+   # cards = for value <- values do 
+   #   for suit <-suits do
+   #     "#{value} of #{suit}"
+   #   end
+   # end
+   # List.flatten(cards)
+
+    for value <-values, suit<-suits do
+      "#{value} of #{suit}"
+    end
+
+  end
+
+  def shuffle(deck)  do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card)do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+  
+    
+  end
+
 end
