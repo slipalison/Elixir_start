94bec6f6b1f896e089d8f927edece8cb06d5823e
slipalison <alisonmsn@hotmail.com>
2017-12-06 15:46:22 -0200

    aula 38 crypto

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -15,4 +15,14 @@ defmodule Identicon do
   def hello do
     :world
   end
+
+
+  def main(input) do
+    input |> hash_input
+  end
+
+  def hash_input(input)do
+    :crypto.hash(:md5, input) |> :binary.bin_to_list
+  end
+
 end
