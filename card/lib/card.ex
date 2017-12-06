0d75abb356870e19ad947fec1f307979be6c1d4d
slipalison <alisonmsn@hotmail.com>
2017-12-06 11:57:49 -0200

    aula 20

--- card/lib/card.ex
+++ card/lib/card.ex
@@ -49,13 +49,16 @@ defmodule Card do
     binary = :erlang.term_to_binary(deck)
     File.write(filename,binary) 
   end
-  def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-      :ok -> :erlang.binary_to_term binary
-      :error -> "That file does not exist"
-    end
 
+  def load(filename) do
+    #{status, binary} = File.read(filename)
+    #case status do
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+    end
   end
+
+
 
 end
